63ba7d2ef2d3a6b75202791a3ffb2ff7d7989597
rafi67 <45172084+rafi67@users.noreply.github.com>
2020-06-02 20:26:40 +0600

    Typed

--- Practice/src/DataStructure/Main.java
+++ Practice/src/DataStructure/Main.java
@@ -1,8 +1,14 @@
 package DataStructure;
 
 public class Main {
-    public void log(int[] numbers){
-        System.out.println(numbers[0]);
-        System.out.println(numbers[0]);
+    public void log(int[] numbers, String[] names){
+        for(int number:numbers){
+            System.out.println(number);
+        }
+        for(int number:numbers){
+            System.out.println(number);
+        }
+        for(String name:names)
+            System.out.println(name);
     }
 }
