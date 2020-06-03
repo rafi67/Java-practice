b5ced888864c98f844b1491e39d0096916c41601
rafi67 <45172084+rafi67@users.noreply.github.com>
2020-06-03 20:26:02 +0600

    Main data structure updated

---
+++ Practice/src/Array/AR.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class AR {
+    public static void main(String Rafi[]){
+        int [] array = new int[5];
+        for(int i=0; i<1; i++){
+            array[i] = 12;
+        }
+        System.out.println(array[0]);
+    }
+}
--- Practice/src/DataStructure/Main.java
+++ Practice/src/DataStructure/Main.java
@@ -2,13 +2,17 @@ package DataStructure;
 
 public class Main {
     public void log(int[] numbers, String[] names){
-        for(int number:numbers){
+        //O(n+n+n)
+        for(int number:numbers) //O (n)
             System.out.println(number);
-        }
-        for(int number:numbers){
+        for(int number:numbers) //O (n)
             System.out.println(number);
-        }
-        for(String name:names)
+        for(String name:names) //O(n)
             System.out.println(name);
+    }
+
+    public static void main (String Rafi[]){
+        Main m = new Main();
+        m.log(new int[]{1,2,3}, new String[] {"Rafi"});
     }
 }
