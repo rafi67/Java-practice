7c51cfa034e050ea3dec121b720e1232d7af43b4
rafi <rafisiddique67@gmail.com>
2020-07-05 16:02:39 +0600

    use of static block

--- Practice/src/JavaMemoryManagement/Emp.java
+++ Practice/src/JavaMemoryManagement/Emp.java
@@ -3,8 +3,13 @@ package JavaMemoryManagement;
 public class Emp {
     int id;
     int salary;
-    static String company; //static
-    String ceo; //non static
+    static String company;
+    String ceo;
+
+    static{
+        company = "SAP";
+        System.out.println(company);
+    }
 
     void showInfo(){
         System.out.println(id+" : "+salary+" : "+company+" : "+ceo);
