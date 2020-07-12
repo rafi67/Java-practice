b100500649c135983eeb7dbb84ea5fa064a184de
rafi <rafisiddique67@gmail.com>
2020-07-12 20:14:48 +0600

    This keyword

---
+++ Practice/src/ThisKeyWord/A.java
@@ -0,0 +1,11 @@
+package ThisKeyWord;
+
+public class A {
+    A getA(){
+        return this;
+    }
+
+    void message(){
+        System.out.println("Hello java");
+    }
+}
---
+++ Practice/src/ThisKeyWord/B.java
@@ -0,0 +1,7 @@
+package ThisKeyWord;
+
+public class B {
+    public static void main(String [] args) {
+        new A().getA().message();
+    }
+}
