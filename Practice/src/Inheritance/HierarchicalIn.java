10c3885a40213041618894cd2fccc2a177aef9b2
rafi <rafisiddique67@gmail.com>
2020-07-13 12:53:28 +0600

    Inheritance

--- Practice/src/Inheritance/HierarchicalIn.java
+++ Practice/src/Inheritance/HierarchicalIn.java
@@ -1,4 +1,27 @@
 package Inheritance;
 
 public class HierarchicalIn {
+    public static void main(String [] args){
+        G g = new G();
+        g.Display();
+        g.Display3();
+    }
+}
+
+class E {
+    public void Display(){
+        System.out.println("Class E");
+    }
+}
+
+class F extends E{
+    public void Display2() {
+        System.out.println("This is class F");
+    }
+}
+
+class G extends E{
+    public void Display3() {
+        System.out.println("This is Class G");
+    }
 }
