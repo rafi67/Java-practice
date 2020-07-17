8a2590d4f344d53395647bf76894509098571194
rafi <rafisiddique67@gmail.com>
2020-07-17 15:22:53 +0600

    Abstraction

--- Practice/src/Abstraction/TestAbstract2.java
+++ Practice/src/Abstraction/TestAbstract2.java
@@ -2,6 +2,16 @@ package Abstraction;
 
 abstract class Shape{
     abstract void draw();
+    final void draw2(){
+        System.out.println("No shape found");
+    }
+    public static void draw3(){
+        System.out.println("NO shape found");
+    }
+
+    private  void draw4(){
+        System.out.println("No shape found");
+    }
 }
 
 class Rectangle extends Shape{
