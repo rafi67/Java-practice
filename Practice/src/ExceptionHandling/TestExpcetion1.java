f794f24018868372a7f528ff71f4e23a51959e62
rafi <rafisiddique67@gmail.com>
2020-07-21 19:38:04 +0600

    Problem solved in CurrencyFormatter

---
+++ Practice/src/ExceptionHandling/TestExpcetion1.java
@@ -0,0 +1,9 @@
+package ExceptionHandling;
+
+public class TestExpcetion1 {
+    private static void show(){}
+
+    public static void main(String [] args){
+        show();
+    }
+}
--- Practice/src/ProblemSolving/CurrencyFormatter.java
+++ Practice/src/ProblemSolving/CurrencyFormatter.java
@@ -2,49 +2,46 @@ package ProblemSolving;
 
 import java.text.*;
 import java.util.*;
+import java.util.Locale;
+import java.io.*;
+import java.math.*;
+import java.util.regex.*;
 
 public class CurrencyFormatter {
+    private static String US(double payment){
+        String mystring = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        return mystring;
+    }
+
+    private static String India(double payment){
+        Locale in = new Locale("EN", "in");
+        String mystring = NumberFormat.getCurrencyInstance(in).format(payment);
+        return mystring;
+    }
+
+    private static String China(double payment){
+        String mystring = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        return mystring;
+    }
+
+    private static String France(double payment){
+        String mystring = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        return mystring;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double payment = scanner.nextDouble();
         scanner.close();
 
-        // Write your code here.
-       // String us;
-       // String india;
-      //  String china;
-        String france = null;
-        try {
-            france = String.valueOf(France(payment));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-        // System.out.println("US: " + us);
-       // System.out.println("India: " + india);
-       // System.out.println("China: " + china);
+        String us =US(payment);
+        String india = India(payment);
+        String china = China(payment);
+        String france = France(payment);
+
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
         System.out.println("France: " + france);
-    }
-
-  /*  public static double America(double us){
-        return us;
-    }
-
-    public static double India(double us){
-        double india = 75.587;
-        double result = india*us;
-        return result;
-    }
-
-    public static double China(double us){
-        double china = 7.0782;
-        double result = us*china;
-        return result;
-    }*/
-
-    public static Number France(double us) throws ParseException {
-        NumberFormat nf = NumberFormat.getInstance(Locale.US);
-        String r = String.valueOf(us);
-            Number myNumber =  nf.parse(r);
-            return myNumber;
     }
 }
