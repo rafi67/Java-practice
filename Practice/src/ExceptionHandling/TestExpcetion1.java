1f21c655b886c8817f81da0239f0c53caa8101bc
rafi <rafisiddique67@gmail.com>
2020-07-22 09:24:56 +0600

    Exception handling

--- Practice/src/ExceptionHandling/TestExpcetion1.java
+++ Practice/src/ExceptionHandling/TestExpcetion1.java
@@ -1,7 +1,14 @@
 package ExceptionHandling;
 
 public class TestExpcetion1 {
-    private static void show(){}
+    private static void show(){
+        try{
+            int data = 100/0;
+        }
+        catch(ArithmeticException e){
+            System.out.println(e);
+        }
+    }
 
     public static void main(String [] args){
         show();
