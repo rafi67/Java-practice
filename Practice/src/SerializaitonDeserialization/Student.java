9766c3ca277b80b6b3aaf1883e24cae8e5cd53d4
rafi <rafisiddique67@gmail.com>
2020-08-11 12:26:27 +0600

    added serialize version uid

--- Practice/src/SerializaitonDeserialization/Student.java
+++ Practice/src/SerializaitonDeserialization/Student.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class Student implements Serializable {
 
-    private static final long serialVersionUID = 1l;
+    private static final long serialVersionUID = 1L;
 
     int id;
     String name;
