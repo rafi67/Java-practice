b2bea056be4556ea3138429d27a8ecdff46635fd
rafi <rafisiddique67@gmail.com>
2020-08-15 22:47:00 +0600

    Month Day

--- Practice/src/DateAndTime/MonthDayDemo.java
+++ Practice/src/DateAndTime/MonthDayDemo.java
@@ -1,6 +1,8 @@
 package DateAndTime;
 
 import java.time.*;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ValueRange;
 
 public class MonthDayDemo {
     private static void monthDay() {
@@ -9,7 +11,30 @@ public class MonthDayDemo {
         System.out.println(date);
     }
 
+    private static void testMonthValid() {
+        MonthDay month = MonthDay.now();
+        boolean b = month.isValidYear(2020);
+        System.out.println(b);
+    }
+
+    private static void gettingMonth() {
+        MonthDay month = MonthDay.now();
+        long n = month.get(ChronoField.MONTH_OF_YEAR);
+        System.out.println(n);
+    }
+
+    private static void dayMonthRange() {
+        MonthDay month = MonthDay.now();
+        ValueRange n = month.range(ChronoField.MONTH_OF_YEAR);
+        System.out.println(n);
+        ValueRange n2 = month.range(ChronoField.DAY_OF_MONTH);
+        System.out.println(n2);
+    }
+
     public static void main(String[]args) {
         monthDay();
+        testMonthValid();
+        gettingMonth();
+        dayMonthRange();
     }
 }
