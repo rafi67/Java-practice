89795b7c2e05d9cf8bd5ce394c0ec528feb8a861
rafis67 <rafisiddique67@gmail.com>
2020-10-01 16:40:43 +0600

    JOptionPane2

--- Practice/src/Swing/DemoJOptionPane.java
+++ Practice/src/Swing/DemoJOptionPane.java
@@ -1,11 +1,14 @@
 package Swing;
 
 import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class DemoJOptionPane extends JFrame {
     DemoJOptionPane(){
         test1();
         test2();
+        test3();
     }
 
     private void test1(){
@@ -28,6 +31,27 @@ public class DemoJOptionPane extends JFrame {
         f.setLocationRelativeTo(null);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setVisible(true);
+    }
+
+    JFrame F;
+
+    private void test3() {
+        F = new JFrame();
+        F.setSize(400,400);
+        F.setLayout(null);
+        F.setLocationRelativeTo(null);
+        F.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                int confirmed =
+                        JOptionPane.showConfirmDialog(null,"Are you sure?",
+                        "Exit",JOptionPane.YES_NO_OPTION);
+                if(confirmed == JOptionPane.YES_OPTION)
+                    F.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            }
+        });
+        F.setVisible(true);
+
     }
 
     public static void main(String... args) {
